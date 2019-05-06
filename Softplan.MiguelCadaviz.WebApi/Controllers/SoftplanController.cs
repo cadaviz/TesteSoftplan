ddb340f97777dc7211096723ff8ed7f80dc859a3
cadaviz <miguelcadaviz@gmail.com>
2019-05-05 21:08:32 -0300

    commit inicial

---
+++ Softplan.MiguelCadaviz.WebApi/Controllers/SoftplanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Softplan.MiguelCadaviz.WebApi.Domain;
+
+namespace Softplan.MiguelCadaviz.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class SoftplanController : Controller
+    {
+
+        [HttpGet("calculajuros")]
+        public decimal CalculaJuros(decimal valorInicial, int meses)
+        {
+            return Calculos.Truncar(Calculos.CalcularJurosCompostos(valorInicial, meses), 2);
+        }
+        
+
+        [HttpGet("showmethecode")]
+        public string ShowMeTheCode()
+        {
+            return @"";
+        }
+    }
+}
---
+++ Softplan.MiguelCadaviz.WebApi/Domain/Calculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Softplan.MiguelCadaviz.WebApi.Domain
+{
+    public static class Calculos
+    {
+        private static readonly decimal _percentualJuros = 0.01m;
+
+        public static decimal CalcularJurosCompostos(decimal valorInicial, int quantidadeMeses)
+        {
+            var valorJuros = (decimal)Math.Pow((double)(1 + _percentualJuros), quantidadeMeses);
+
+            return valorInicial * valorJuros;
+        }
+        
+        public static decimal Truncar(decimal valor, int casasDecimais)
+        {
+            long multiplicador = (long)Math.Pow(10, casasDecimais);
+
+            return Math.Truncate(valor * multiplicador ) / multiplicador;
+        }
+    }
+}
